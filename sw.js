830cfb3136d833d06b579ff06a54c55f3329001a
Incipient <incipient@Incipients-MacBook-Air.local>
2026-02-27 07:35:07 -0500

    Fix domain filter: s.domain -> s.category

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ems-national-v5';
+const CACHE_NAME = 'ems-national-v6';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
