f3af165865a8f63ada4cc7eba92c7ae0f0732e62
Incipient <incipient@Incipients-MacBook-Air.local>
2026-02-27 08:34:04 -0500

    Reorder: level -> button -> difficulty -> domain; fix chip visibility

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ems-national-v9';
+const CACHE_NAME = 'ems-national-v10';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
